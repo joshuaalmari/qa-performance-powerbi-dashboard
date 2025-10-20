88dff0eaf8062cda11c6ffba6da987f832aa2108
Engr. Joshua Almari, ECT <138535183+joshuaalmari@users.noreply.github.com>
2025-10-20 23:20:11 +0800

    Add files via upload

---
+++ sql-pipeline/09_qa_mart_errors_final.sql
@@ -0,0 +1,34 @@
+-- Drop existing version if any
+DROP MATERIALIZED VIEW IF EXISTS mart.mv_errors;
+
+-- Create materialized view (uniform with mv_accuracy)
+CREATE MATERIALIZED VIEW mart.mv_errors AS
+SELECT
+    e.error_detail_new                                  AS "Error_Detail_New",
+    e.site                                              AS "Site",
+    e.powerappsid                                       AS "__PowerAppsId__",
+    e.error_category_new                                AS "Error_Category_New",
+    e.globalid                                          AS "GlobalID",
+    e.firstname                                         AS "FirstName",
+    e.lastname                                          AS "LastName",
+    e.remotepartner                                     AS "RemotePartner",
+    e.role                                              AS "Role",
+    e.team_lead                                         AS "Team Lead",
+    e.domesticteamlead1                                 AS "DomesticTeamLead1",
+    e.domesticteamlead2                                 AS "DomesticTeamLead2",
+    e.sales_order_number                                AS "Sales Order Number",
+    e.score                                             AS "Score",
+    e.auditor                                           AS "Auditor",
+    e.datetimestamp                                     AS "DateTimeStamp",
+    e.role_selection                                    AS "Role_Selection",
+    e.week_beginning                                    AS "Week Beginning",
+    e.process_name                                      AS "Process Name",
+    concat_ws(' ', trim(e.firstname), trim(e.lastname)) AS "Name",
+    e.error_type                                        AS "Audit Type"
+FROM staging.vw_errors_exploded e
+ORDER BY e.datetimestamp, e.powerappsid, e.error_category_new, e.error_detail_new;
+
+-- Preview sample rows
+SELECT *
+FROM mart.mv_errors
+LIMIT 5;
